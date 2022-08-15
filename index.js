09cdc3861d4b53a9c240916e69a69c6bc5184075
Simion <simion.okola@student.moringaschool.com>
2022-08-16 02:01:34 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,37 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(ride){
   // Write your code here!
+  if(ride <= 400){
+    return 'This one is on me!';
+  }
+  else if(ride > 2000 && ride <=2500 ){
+    return 'I will gladly take your thirty bucks.';
+  }
+  else if (ride >2500){
+    return 'No can do.';
+  }
 }
+scuberGreetingForFeet(2501);
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
 
-function switchOnCharmFromTip(){
+function ternaryCheckCity(city){
   // Write your code here!
+ const destination = city === 'NYC'? "Ok, sounds good." : "No go.";
+ return destination;
 }
+ternaryCheckCity('NYC');
+
+
+
+function switchOnCharmFromTip(tip){
+  // Write your code here!
+  switch(tip){
+    case 'generous':
+    return 'Thank you so much.';
+    case 'not as generous':
+    return 'Thank you.';
+    break;
+    default: 
+    return 'Bye.';
+  }
+}
+switchOnCharmFromTip('thanks for everything');
